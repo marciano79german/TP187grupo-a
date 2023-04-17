0d6f9df2b3d42a437374572369f879f5e1ee2cf0
donjuancrs <128987192+donjuancrs@users.noreply.github.com>
2023-04-17 14:51:17 -0300

    Intento de restauración
    Tratando de restarurar lo que se borró

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+document.addEventListener("DOMContentLoaded",() =>{
+    var botonA = document.getElementById("botonAnterior");
+    var botonB = document.getElementById("botonSiguiente");
+    botonA.addEventListener("click",() =>{
+        var slice = document.getElementsByClassName("divSlice");
+        var imgActiva = slice.getElementsByClassName(".activada");
+        alert(imgActiva);
+    });
+    botonB.addEventListener("click",() =>{
+        var slice = document.getElementsByClassName("divSlice");
+        var imgActiva = slice.getElementsByClassName(".activada");    
+        alert(imgActiva);
+    });
+});
